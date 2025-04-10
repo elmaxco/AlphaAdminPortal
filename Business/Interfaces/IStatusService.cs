79465f5c0d0806a30349d96d18402c31cb8b6456
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2025-04-10 14:07:40 +0200

    Add status management DTOs and service implementation
    Introduced `StatusDto` and `StatusUpdateDto` for status data representation. Added an empty `StatusFactory` class for future entity creation. Defined `IStatusService` interface with methods for CRUD operations on status entities. Implemented `StatusService` to handle these operations, including error handling and logging, while utilizing the repository pattern and the `StatusFactory` for DTO conversion.

---
+++ Business/Interfaces/IStatusService.cs
@@ -0,0 +1,10 @@
+﻿namespace Business.Interfaces;
+
+public interface IStatusService
+{
+    Task<int> CreateStatusAsync(AddStatusForm form);
+    Task<bool> DeleteStatusAsync(Status status);
+    Task<StatusDto?> GetStatusAsync(int id);
+    Task<IEnumerable<StatusDto?>> GetStatusesAsync();
+    Task<bool> UpdateStatusAsync(StatusUpdateDto dto);
+}
