d18be01e481d740dfd85e32a6cb963fba340b072
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2025-04-10 16:08:54 +0200

    Enhance AdminController and update view models
    - Added IStatusService to AdminController and new action methods for project retrieval and status display.
    - Removed ProjectViewModel.cs, indicating a restructuring of project data management.
    - Updated Clients.cshtml to use ClientDto and improved layout for displaying client details.
    - Modified Projects.cshtml to use ProjectDto, added filtering options, and changed layout to a card format.
    - Introduced LoginViewModel and SignUpViewModel with validation attributes for user authentication.

--- WebApp/Controllers/AdminController.cs
+++ WebApp/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Interfaces;
+using Business.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
@@ -11,11 +12,15 @@ public class AdminController : Controller
 {
     private readonly IProjectService _projectService;
     private readonly IClientService _clientService;
+    private readonly IStatusService _statusService;
 
-    public AdminController(IProjectService projectService, IClientService clientService)
+
+    public AdminController(IProjectService projectService, IClientService clientService, IStatusService statusService)
     {
         _projectService = projectService;
         _clientService = clientService;
+        _statusService = statusService;
+
     }
 
     [Route("projects")]
@@ -25,10 +30,24 @@ public class AdminController : Controller
         return View();
     }
 
+    [HttpGet("projects/{id}")]
+    public async Task<IActionResult> GetProject(int id)
+    {
+        var project = await _projectService.GetProjectAsync(id);
+        return project != null ? Ok(project) : NotFound();
+    }
+
     [Route("clients")]
     public async Task<IActionResult> Clients()
     {
         var clients = await _clientService.GetAllClientsAsync();
         return View();
-    }    
+    }
+
+    [Route("statuses")]
+    public async Task<IActionResult> Statuses()
+    {
+        var statuses = await _statusService.GetStatusesAsync();
+        return View("statuses", statuses);
+    }
 }
