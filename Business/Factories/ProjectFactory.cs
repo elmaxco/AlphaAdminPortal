fb9b65978a4a8fbac25557515cdc23370399e6b2
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2025-04-10 17:40:32 +0200

    Refactor ProjectFactory namespace and clean up usings
    Removed unnecessary  directives

--- Business/Factories/ProjectFactory.cs
+++ Business/Factories/ProjectFactory.cs
@@ -1,12 +1,8 @@
 ﻿using Business.Dtos;
 using Business.Models;
 using Data.Entities;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Business.Factories;
 
